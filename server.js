e7bae04b3dbafb8f176d1e28a4bd663888056d7c
Sandeep <sandeep.se2021ecec@sece.ac.in>
2023-08-08 18:58:06 +0530

    a

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://todo-list-mern-phi.vercel.app/',
+  origin: 'https://todo-list-mern-phi.vercel.app',
   credentials: true,
 }));
 
