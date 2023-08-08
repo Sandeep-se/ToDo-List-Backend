437de2609bdbefc4f4d053a53d15234f0548ccef
Sandeep <sandeep.se2021ecec@sece.ac.in>
2023-08-08 17:52:42 +0530

    a

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   credentials: true,
 }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://todo-list-mern-phi.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'application/json, text/plain, */*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
