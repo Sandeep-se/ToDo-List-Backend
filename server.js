28e2e142fc84e2ca21b46b365a459fabe69d31b4
Sandeep <sandeep.se2021ecec@sece.ac.in>
2023-08-10 19:23:44 +0530

    a

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'https://todo-list-ruddy-two.vercel.app',
+  origin: 'https://main--playful-truffle-ca2aa1.netlify.app',
   credentials: true,
 }));
 
